6d494fb91721bfd01fafd0571af398069e2d3845
coreyapat <coreyapat19@gmail.com>
2021-06-14 11:07:51 -0500

    added and created dinosaurs and an init

---
+++ herd.py
@@ -0,0 +1,15 @@
+from dinosaur import Dinosaur
+
+class Mammal:
+    def __init__(self):
+        self.dinosaur = []
+        self.create_dinosaur()
+
+
+    def create_dinosaur(self):
+        rex = Dinosaur("Rex")
+        raptor = Dinosaur("Raptor")
+        long_neck = Dinosaur("Long Neck")
+        
+
+
